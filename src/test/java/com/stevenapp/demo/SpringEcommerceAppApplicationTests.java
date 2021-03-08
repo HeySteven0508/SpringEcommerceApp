fa9b938bdaf088b8b88b9450122e5bc797094070
HeySteven0508 <concentratify@gmail.com>
2021-03-08 23:42:50 +0800

    Bootstrapping the application.

---
+++ src/test/java/com/stevenapp/demo/SpringEcommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stevenapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
